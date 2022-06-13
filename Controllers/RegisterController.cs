9d62127fc3b40b61b3bcbce0c866ab61f363ed6e
Евгений Гилев <euggil@yandex.ru>
2022-06-14 00:54:50 +0500

    Добавил ViewModel RegisterView, View Register и Register2, контроллер RegisterController, внес изменения в index

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeNetworkM35.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeNetworkM35.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
