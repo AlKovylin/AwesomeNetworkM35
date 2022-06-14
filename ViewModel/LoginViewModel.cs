dc76d74bb61656f641c5710bed1dd6a41344468b
Евгений Гилев <euggil@yandex.ru>
2022-06-15 00:17:51 +0500

    Добавил форму логина пользователя

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwesomeNetworkM35.ViewModel
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string EmailLogin { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль", Prompt = "Введите пароль")]
+        public string PasswordLogin { get; set; }
+
+        [Display(Name = "Запомнить учетные данные?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
