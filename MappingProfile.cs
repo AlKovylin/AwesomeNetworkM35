9314f841c5eab44ab4819a54e0ad3c045f65ac4c
Евгений Гилев <euggil@yandex.ru>
2022-06-15 01:20:40 +0500

    на форме регистрации добавил выпадающие списки на дате рождения (месяцы)

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using AwesomeNetworkM35.Models;
+using AwesomeNetworkM35.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,11 @@ namespace AwesomeNetworkM35
     {
         public MappingProfile()
         {
-
+            CreateMap<RegisterViewModel, User>()
+                .ForMember(x => x.BirthDate, opt => opt.MapFrom(c => new DateTime((int)c.Year, (int)c.Month, (int)c.Date)))
+                .ForMember(x => x.Email, opt => opt.MapFrom(c => c.EmailReg))
+                .ForMember(x => x.UserName, opt => opt.MapFrom(c => c.Login));
+            CreateMap<LoginViewModel, User>();
         }
     }
 }
