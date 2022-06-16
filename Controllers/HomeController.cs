b36d9c8f87d933f82a7970207524a0256a189a4f
Евгений Гилев <euggil@yandex.ru>
2022-06-16 21:41:13 +0500

    добавили лого картинку, переход на страницу пользователя через ллго

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,31 @@
-﻿using AwesomeNetworkM35.ViewModel.Account;
+﻿using AwesomeNetworkM35.Models.Users;
+using AwesomeNetworkM35.ViewModel.Account;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AwesomeNetworkM35.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<User> _signInManager;
+
+        public HomeController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [Route("")]
         [Route("[controller]/[action]")]
         public IActionResult Index()
         {
-            return View(new MainViewModel());
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("MyPage", "AccountManager");
+            }
+            else
+            {
+                return View(new MainViewModel());
+            }
         }
 
         [Route("[action]")]
